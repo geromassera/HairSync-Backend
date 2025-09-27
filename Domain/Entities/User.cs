513fecfcfd4591979e97a3c4febb6bf82556e9ba
geromassera <gerocabrer37@gmail.com>
2025-09-27 16:53:42 -0300

    Creado User, UserDto, IUserService, IRepositoryBase, IUserRepository, enum de UserRole

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(25)]
+        public string Surname { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(60)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(255)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        public UserRole Role { get; set; }
+    }
+}
